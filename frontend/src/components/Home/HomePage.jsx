6dc30e59a4a9fed3e0b4007d0cab456010366d48
Sujoy Paul <freelancersujoy@gmail.com>
2024-09-02 00:16:56 +0600

    💭 mongoose setup and first model

--- frontend/src/components/Home/HomePage.jsx
+++ frontend/src/components/Home/HomePage.jsx
@@ -13,7 +13,7 @@ const HomePage = () => {
             <div className='flex justify-center items-center mx-auto w-full'>
                 <button onClick={() => setCounter(counter-1)} className='font-bold text-[80px] mr-20 inline-block'>-</button>
                 <h2 className='text-[80px] text-[#BA5354] inline-block'>{counter}</h2>
-                <button onClick={() => setCounter(counter+1)} className='font-bold text-[80px] ml-20 inline-block'>+</button>
+                <button onClick={() => setCounter(counter+5)} className='font-bold text-[80px] ml-20 inline-block'>+</button>
             </div>
             <h1 className='text-6xl uppercase px-20 py-10 text-[#BA5354]'>- Welcome to your new project - </h1>
             <p className='text-[20px] mb-16 font-bold px-20 uppercase text-[#BA5354]'>Kaiser - Abdullah - Sujoy - Naeem</p>
