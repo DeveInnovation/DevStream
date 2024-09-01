d6d4eb97f5dfd88aadaf8cfdb967e0788dd93a85
Your Name <your.email@example.com>
2024-09-02 00:49:33 +0600

    Hero section added

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,11 @@
 import HomePage from "@/components/Home/HomePage";
+import Navbar from "@/SharedComponent/Navbar/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+        <Navbar/>
        <HomePage></HomePage>
     </div>
   );
