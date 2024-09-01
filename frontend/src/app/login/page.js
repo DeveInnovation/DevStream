8bdbaef5498d3f45718e48ecc26a131c4843df12
abdullah00001 <abdullahbinomarchowdhury02@gmail.com>
2024-09-01 22:01:44 +0600

    login & signup page added

---
+++ frontend/src/app/login/page.js
@@ -0,0 +1,11 @@
+"use client"
+import LogIn from '@/components/Authentication/LogIn'
+import React from 'react'
+
+function page() {
+  return (
+    <LogIn></LogIn>
+  )
+}
+
+export default page
---
+++ frontend/src/components/Authentication/LogIn.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LogIn() {
+  return <div className="text-6xl text-center mt-[500px]">LogIn Page</div>;
+}
+
+export default LogIn;
