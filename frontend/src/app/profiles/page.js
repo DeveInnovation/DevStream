4cda7bd7475ff00aec529da0068a0953ec0ff6cc
Sujoy Paul <freelancersujoy@gmail.com>
2024-09-01 21:41:50 +0600

    update

--- frontend/src/app/profiles/page.js
+++ frontend/src/app/profiles/page.js
@@ -5,12 +5,12 @@ import HomePage from "@/components/Home/HomePage";
 
 const ProfilePage = async () => {
 
-    const res = await fetch(`https://helphand.vercel.app/cards?findtext=`)
+    const res = await fetch(`https://profile-view-be.vercel.app/profile/@rahul`, {cache: 'no-store'})
     const data = await res.json()
     console.log(data);
     return (
         <div>
-            <h1 className='text-xl text-center py-2 uppercase bg-[#BA5354] text-white'>Profile page</h1>
+            <h1 className='text-xl text-center py-2 uppercase bg-[#BA5354] text-white'>Profile page - {data.username}</h1>
             <HomePage></HomePage>
         </div>
     );
