c2118ba67bce3a2df27f2e7784a2e6b8a27abcfd
Sujoy Paul <freelancersujoy@gmail.com>
2024-09-02 18:08:47 +0600

    authorization system with jwt and secure pass with bycrypt

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,13 @@ const port = process.env.PORT || 5000
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config()
-
 // require models 
 const {User} = require('./models/registration');
+// -------------------------------------------------
 
 // require controllers 
 const createUser = require('./controllers/registration/ct_createuser')
+// -------------------------------------------------
 
 app.use(express.json())
 app.use(cors())
@@ -17,13 +18,17 @@ app.use(cors())
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@devinnovationdb.rcz6p.mongodb.net/DevStream?retryWrites=true&w=majority&appName=DevInnovationDB`)
   .then(() => console.log('Database connected!'));
   
-  
-app.post('/createuser', createUser)
+  //create a user
+  app.post('/createuser', createUser)
+  // app 
 
-  
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+
+
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
