f38752443dde1c9976059b95794afe446aca1b30
abdullah00001 <abdullahbinomarchowdhury02@gmail.com>
2024-09-04 14:32:20 +0600

    integrate login api in login page

--- frontend/src/components/Authentication/LogIn.jsx
+++ frontend/src/components/Authentication/LogIn.jsx
@@ -1,20 +1,47 @@
+"use client";
+import axios from "axios";
 import Link from "next/link";
-import React from "react";
 const loginImg = "/assets/login.jpg";
 import { FaStarOfLife } from "react-icons/fa";
-
-const handleLogin=(event)=>{
-  event.preventDefault()
-  const form=event.target
-  const email=form.email.value
-  const password=form.password.value
-  const userCredentials={email,password}
-  console.log(userCredentials);
-}
+import Cookies from "js-cookie";
+import { ThreeDots } from "react-loader-spinner";
+import React, { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 function LogIn() {
+  const [error, setError] = useState(false);
+  const [Messages, setMessages] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleLogin = (event) => {
+    setLoading(true);
+    setError(false);
+    setMessages(null);
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const userCredentials = { email, password };
+    axios
+      .post("https://devstream-server.vercel.app/userlogin", userCredentials)
+      .then((response) => {
+        if (response.status === 200 && response.data.token) {
+          setLoading(false);
+          toast.success("Log in Successful");
+          Cookies.set("sessiontoken", response.data.token);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.status === 404) {
+          setLoading(false);
+          setError(true);
+          setMessages(error.response.data.error);
+        }
+      });
+  };
   return (
     <section className="w-full md:flex">
+      <Toaster position="top-center" reverseOrder={false} />
       <div className="md:w-[50%] absolute md:relative md:z-0 -z-10 w-full h-screen overflow-hidden">
         <div
           className="w-full"
@@ -32,7 +59,7 @@ function LogIn() {
           <div className="">
             <div>
               <h1 className="font-medium text-[24px] leading-7 text-[#444444]">
-                Create your Account
+                Log In
               </h1>
               <p className="font-normal mt-3 text-[12px] leading-4 text-[#4a4a4a]">
                 Figma ipsum component variant main layer.
@@ -81,17 +108,34 @@ function LogIn() {
                     Forget Password?
                   </Link>
                 </div>
-                <div className="w-full">
+                <div className="w-full mt-4">
+                  {error && (
+                    <p className="text-red-500 pl-2 text-sm mb-2">{Messages}</p>
+                  )}
                   <button
                     type="submit"
-                    className="bg-[#21005D] text-[#ffffff] text-[14px] font-medium leading-5 text-center py-[10px] w-full rounded-[100px] mt-[32px]"
+                    className="bg-[#21005D] text-[#ffffff] text-[14px] flex justify-center font-medium leading-5 text-center py-[10px] w-full rounded-[100px] "
                   >
-                    Log In
+                    {loading ? (
+                      <ThreeDots
+                        visible={true}
+                        height="20"
+                        width="30"
+                        color="#ffffff"
+                        radius="9"
+                        ariaLabel="three-dots-loading"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                      />
+                    ) : (
+                      "Log In"
+                    )}
                   </button>
                 </div>
                 <div className="w-full">
                   <h5 className="text-[14px] leading-5 font-medium text-[#444444] mt-4">
-                    If You Are New! Please <Link className="text-green-500 underline" href="/signup">
+                    If You Are New! Please{" "}
+                    <Link className="text-green-500 underline" href="/signup">
                       sign up
                     </Link>
                   </h5>
