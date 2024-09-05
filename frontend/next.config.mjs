b4563f2263c1b4d19661e14e381194ff9443589a
Sujoy Paul <freelancersujoy@gmail.com>
2024-09-05 14:16:40 +0600

    login redirection

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,6 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      // Enable middleware support
-      middleware: true,
-    },
-    // Define path matchers if needed
-    matcher: ['/login*', '/profile/:path*'],
   };
   
   export default nextConfig;
