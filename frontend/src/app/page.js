ea51c67de8837065962ea7f8b7dc8fa5b2aab241
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2024-09-12 20:54:20 +0600

    User about and contact added

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,12 +1,17 @@
 import HomePage from "@/components/Home/HomePage";
+import UserAbout from "@/components/UserAbout/UserAbout";
+import UserContact from "@/components/UserContacts/UserContact";
+import UserProfile from "@/components/UserProfile/UserProfile";
 import Navbar from "@/SharedComponent/Navbar/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full overflow-x-hidden">
-        <Navbar></Navbar>
-       <HomePage></HomePage>
+    <div className="w-full overflow-x-hidden bg-[#F9FAFB]">
+        <Navbar/>
+       <UserProfile/>
+       <UserAbout/>
+       <UserContact/>
     </div>
   );
 }
